c3e8b50edf7cf3fbb49063588109dc054aa3d054
Bruno Luz <bsilva.se@gmail.com>
2021-10-21 23:02:14 -0300

    main - Primeiros passos com CSharp

---
+++ BSTK.SistemaBancario/Gerente.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace BSTK.SistemaBancario
+{
+    class Gerente
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Cpf{ get; set; }
+
+        public void AprovarHoraFuncionario(Funcionario funcionario)
+        {
+            if (funcionario is null)
+            {
+                Console.WriteLine("Dados inválidos!");
+                return;
+            }
+
+            ValidarCpf(funcionario.Cpf);
+
+            Console.WriteLine("Aprovando horas para funcionario " + funcionario.Nome + " de CPF: " + funcionario.Cpf);
+
+        }
+
+        public void AprovarHoraFuncionario(string cpf)
+        {
+            ValidarCpf(cpf);
+
+            Console.WriteLine("Aprovando horas para funcionario de CPF: " + cpf);
+        }
+
+        private void ValidarCpf(string cpf)
+        {
+            if (cpf is null || "".Equals(Cpf))
+            {
+                Console.WriteLine("CPF Invalido!");
+                return;
+            }
+        }
+    }
+}
---
+++ BSTK.SistemaBancario/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BSTK.SistemaBancario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("** Sistema Bancário **");
+
+            var funcionarioAlberto = new Funcionario();
+            funcionarioAlberto.Nome = "Alberto Souza";
+            funcionarioAlberto.Cpf = "393.112.999/88";
+
+            var funcionarioAdalto = new Funcionario();
+            funcionarioAdalto.Nome = "Adalto Pereira";
+            funcionarioAdalto.Cpf = "111.222.333/66";
+
+            var gerente = new Gerente();
+
+            Console.WriteLine("> Método somente CPF");
+            gerente.AprovarHoraFuncionario(funcionarioAdalto.Cpf);
+            gerente.AprovarHoraFuncionario(funcionarioAlberto.Cpf);
+
+            Console.WriteLine();
+
+            Console.WriteLine("> Método objeto");
+            gerente.AprovarHoraFuncionario(funcionarioAdalto);
+            gerente.AprovarHoraFuncionario(funcionarioAlberto);
+
+            Console.ReadKey();
+        }
+    }
+}
