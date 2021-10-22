0b6c88ddfce14fe50b46687a004a1a6b39313c56
Bruno Luz <bsilva.se@gmail.com>
2021-10-21 23:11:07 -0300

    main - Refatorando método main

--- BSTK.SistemaBancario/Program.cs
+++ BSTK.SistemaBancario/Program.cs
@@ -4,7 +4,7 @@ namespace BSTK.SistemaBancario
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("** Sistema Bancário **");
 
